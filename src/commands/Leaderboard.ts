bc8b418e3f9bcce7c2f61531967849976cfc44fc
raziman <raziman.terra@gmail.com>
2021-10-18 21:11:39 +0800

    add Leaderboard command

---
+++ src/commands/Leaderboard.ts
@@ -0,0 +1,38 @@
+import { Command } from "@jiman24/commandment";
+import { Message, MessageEmbed } from "discord.js";
+import { User } from "../db/User";
+import { Player } from "../structure/Player";
+
+export default class Leaderboard extends Command {
+  name = "leaderboard";
+  aliases = ["l", "lb"];
+
+  async exec(msg: Message) {
+
+
+    const userDocs = await User.find();
+    const players: Player[] = [];
+
+    for (const userDoc of userDocs) {
+
+      const user = await msg.client.users.fetch(userDoc.userID);
+
+      if (user) {
+        players.push(new Player(user, userDoc));
+      }
+    }
+    
+    players.sort((a, b) => b.user.balance - a.user.balance);
+
+    const list = players
+      .map((x, i) => `${i + 1}. ${x.name} ${x.user.balance} :taco:`)
+      .join("\n");
+
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Leaderboard")
+      .setDescription(list)
+
+    msg.channel.send({ embeds: [embed] });
+  }
+}
