196de4df2438ab24296e7b8eb9ff5c08378592a8
raziman <raziman.terra@gmail.com>
2021-10-18 15:11:08 +0800

    add Price command

---
+++ src/commands/Price.ts
@@ -0,0 +1,30 @@
+import { Command } from "@jiman24/commandment";
+import { Message } from "discord.js";
+import { Player } from "../structure/Player";
+import { validateAmount, validateNumber } from "../utils";
+
+export default class Price extends Command {
+  name = "price";
+
+  async exec(msg: Message, args: string[]) {
+
+    try {
+
+      const player = await Player.fromUser(msg.author);
+      const amount = parseInt(args[0]);
+
+      validateNumber(amount);
+      validateAmount(amount, player.user.balance);
+
+      player.user.price = amount;
+
+      await player.user.save();
+
+      msg.channel.send(`successfully set ${amount} :taco: as your marry price`);
+
+    } catch (err) {
+      
+      msg.channel.send((err as Error).message);
+    }
+  }
+}
