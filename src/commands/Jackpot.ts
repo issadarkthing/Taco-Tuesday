6ed78c3996d76925e2479e8b0c45d5eeb8b46e30
raziman <raziman.terra@gmail.com>
2021-10-19 11:32:02 +0800

    add Jackpot command

---
+++ src/commands/Jackpot.ts
@@ -0,0 +1,16 @@
+import { Command } from "@jiman24/commandment";
+import { Message } from "discord.js";
+import { Jackpot } from "../db/Jackpot";
+
+export default class extends Command {
+  name = "jackpot";
+  description = "show current jackpot";
+
+  async exec(msg: Message) {
+
+    const jackpot = await Jackpot.getMain();
+
+    msg.channel.send(`Current jackpot is ${jackpot.amount} :taco:`);
+
+  }
+}
