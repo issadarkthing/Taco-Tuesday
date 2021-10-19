50af29fcd3a0dd0e34ce682296c7542eeecff89f
raziman <raziman.terra@gmail.com>
2021-10-19 12:42:07 +0800

    add Weekly command

---
+++ src/commands/Weekly.ts
@@ -0,0 +1,30 @@
+import { Command } from "@jiman24/commandment";
+import { Message } from "discord.js";
+import { Player } from "../structure/Player";
+import { DateTime } from "luxon";
+
+export default class extends Command {
+  name = "weekly";
+  reward = 400;
+
+  async exec(msg: Message) {
+
+    const player = await Player.fromUser(msg.author);
+    const lastClaim = DateTime.fromJSDate(player.user.lastClaim.weekly);
+    const now = DateTime.now();
+    const diff = now.diff(lastClaim, ["weeks"]);
+
+    if (diff.weeks > 1) {
+      
+      player.user.balance += this.reward;
+      player.user.lastClaim.weekly = now.toJSDate();
+      msg.channel.send(`Claimed weekly reward! Earned ${this.reward} :taco:`);
+      player.user.save();
+
+    } else {
+
+      msg.channel.send(`You already claimed for this week`);
+  
+    }
+  }
+}
